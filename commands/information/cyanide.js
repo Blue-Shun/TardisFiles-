0259e184061a5bf8bef30bb772447d5a3073e571
Rhiannon <36514736+Blue-Shun@users.noreply.github.com>
2018-06-04 20:32:47 +0100

    Update cyanide.js

--- commands/information/cyanide.js
+++ commands/information/cyanide.js
@@ -1,1 +1,5 @@
-
+{
+    "title": "Cyanide",
+    "description": "Cyanide was a white powder which was poisonous to many species. It killed by inhibiting cellular respiration. Symptoms included headache, nausea and convulsions. Cyanide poisoning could be reversed with ethylenediaminetetraacetic acid.",
+    "thumbnail": "https://vignette.wikia.nocookie.net/tardis/images/8/8b/Detox.jpg/revision/latest/scale-to-width-down/180?cb=20080621193841"
+}
