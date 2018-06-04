208bb606d4ce3efa79d65155032444f0db04305a
Rhiannon <36514736+Blue-Shun@users.noreply.github.com>
2018-06-04 20:35:09 +0100

    Update info-list.js

--- commands/info-list.js
+++ commands/info-list.js
@@ -5,7 +5,7 @@
             name: "Info List",
             icon_url: client.user.avatarURL
           },
-          description: "Name - Command\nCaptain Jack - captain-jack\nConfession Dial - cd\nOhilia - ohilia\nRiver Song/Melody pond - river\nThe TARDIS - tardis\nRory Williams - rory",
+          description: "Name - Command\nCaptain Jack - captain-jack\nConfession Dial - cd\nOhilia - ohilia\nRiver Song/Melody pond - river\nThe TARDIS - tardis\nRory Williams - rory\n Cyanide - cyanide",
           timestamp: new Date(),
           footer: {
             icon_url: message.author.avatarURL,
