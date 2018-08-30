30ca44d140e903a5d64e0914ba44c402b749a601
Rhiannon <36514736+Blue-Shun@users.noreply.github.com>
2018-08-30 16:46:22 +0100

    Update about.js

--- commands/about.js
+++ commands/about.js
@@ -5,7 +5,7 @@
             name: "About",
             icon_url: client.user.avatarURL
           },
-          description: `Name: ${client.user.tag}\nServer count: ${client.guilds.size}\nOwners: Blue Shun #7167 & Fubbo #0487\nPing: ${Math.floor(client.ping * 0.001)}s\nUptime: ${Math.floor(client.uptime * 1.66667e-5)}mins or ${Math.floor(client.uptime * 2.77778e-7)}hours`,
+          description: `Name: ${client.user.tag}\nServer count: ${client.guilds.size}\nOwners: Blue Shun #2318\nPing: ${Math.floor(client.ping * 0.001)}s\nUptime: ${Math.floor(client.uptime * 1.66667e-5)}mins or ${Math.floor(client.uptime * 2.77778e-7)}hours`,
           timestamp: new Date(),
           footer: {
             icon_url: message.author.avatarURL,
